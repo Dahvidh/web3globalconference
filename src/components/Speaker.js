dc73f9d56e8cd53b7d2b4e15b665ac3a33341a4d
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2023-10-26 01:01:55 +0100

    Update Speaker.js

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -1,49 +1,326 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react'
 
 function Speaker() {
-  useEffect(() => {
-    axios.get('https://app.web3trainingteam.com/w3gc/speaker/')
-      .then(response => console.log(response.data))
-      .catch(error => console.error('ERROR', error));
-  }, []);
-
   return (
-    <>
-      <div className="speaker-form-header">
-        <h1>W3GC Speaker's Application</h1>
-        <p>
-          <em>
-            Want to be a Speaker for our global conference? Fill out this form.
-          </em>
-        </p>
-      </div>
-
-      <form action="/submit" method="POST" className="speaker-form">
-        <div className="form-row">
-          <label htmlFor="full-name">Name</label>
-          <input id="full-name" name="full-name" type="text" />
-        </div>
-        <div className="form-row">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            placeholder="joe@example.com"
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="abstract">Topic</label>
-          <textarea id="abstract" name="abstract" />
-          <div className="instructions">
-            Describe your talk in 500 words or less.
+    <div id="Key-Speakers" className="section_key-speakers">
+  <div className="padding-global">
+    <div className="padding-section-large">
+      <div className="container-large">
+        <div className="speaker_wrapper">
+          <div className="headline-wrapper-4rem">
+            <div className="margin-bottom margin-medium">
+              <h2>Keynote Speakers</h2>
+            </div>
+            <p className="text-size-large">
+              Exclusive mix of Speakers &amp; Panelists: bringing together top
+              industry leaders
+            </p>
           </div>
+          <div className="team-grid_wrapper w-dyn-list">
+            <div role="list" className="team-grid w-dyn-items">
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Vaibhav Ali.jpeg"
+                      alt="Vaibhav Ali"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Vaibhav Ali</div>
+                  <div className="team-member-position">Growth, Content Creator</div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Photo_Peter Hui_White.jpg"
+                      alt="Peter Hui"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Peter Hui</div>
+                  <div className="team-member-position">
+                    Moongate, Co-founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Mudit Marda.jpg"
+                      alt="Mudit Marda"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Mudit Marda</div>
+                  <div className="team-member-position">
+                    DRIFE & DexAuction, Founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Morten Rongaard.jpg"
+                      alt="Morten Rongaard"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Morten Rongaard</div>
+                  <div className="team-member-position">
+                    REALITY+, Co-Founder and CEO
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Jerop Joan.jpeg"
+                      alt="Jerop Joan"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Jerop Joan</div>
+                  <div className="team-member-position">
+                    MEMOI, Founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Frank Deya Profile Photo.jpg"
+                      alt="Frank Deya"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Frank Deya</div>
+                  <div className="team-member-position">
+                    FOMOWEEKLY, Director
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Astha Yadav.jpg"
+                      alt="Astha Yadav"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Astha Yadav</div>
+                  <div className="team-member-position">Zero-knowledge advocate</div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Dimitri Tsapis.jpeg"
+                      alt="Dimitri Tsapis"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Dimitri Tsapis</div>
+                  <div className="team-member-position">
+                   FORCEFI, CMO
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Moran Hertzanu weiss Res.jpg"
+                      alt="Moran Hertzanu weiss"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Moran Weiss</div>
+                  <div className="team-member-position">Hedera, Founder</div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Jeff Mkungusi.jpeg"
+                      alt="Jeff Mkungusi"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Jeff Mkungusi</div>
+                  <div className="team-member-position">
+                   Africa Blockchain Center, CEO
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Sakthi V Rajaguru.jpg"
+                      alt="Sakthi V Rajaguru"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Sakthi V Rajaguru</div>
+                  <div className="team-member-position">
+                   FABC, Co-founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Nick Majumdar.jpg"
+                      alt="Nick Majumdar"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Nick Majumdar</div>
+                  <div className="team-member-position">
+                    BlockX and Tokenizer, Founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Nikhil Goyal.jpeg"
+                      alt="Nikhil Goyal"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Nikhil Goyal</div>
+                  <div className="team-member-position">
+                    Beyond Imagination, Founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Kaya Gulten.jpg"
+                      alt="Kaya Gulten"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Kaya Gulten</div>
+                  <div className="team-member-position">PITCHTALK, Business Developer</div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Pranav.jpg"
+                      alt="Pranav Agarwal "
+                       className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Pranav Agarwal</div>
+                  <div className="team-member-position">
+                    FOUNDERSHIP, Head of Capital
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Shahd Abdelhadi.jpg"
+                      alt="Shahd Abdelhadi"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Shahd Abdelhadi</div>
+                  <div className="team-member-position">Takadao, Head of Growth</div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Tomal K Ganguly.jpg"
+                      alt="Tomal K Ganguly"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Tomal K Ganguly</div>
+                  <div className="team-member-position">
+                   Centauri Web3 Consortium, Co-founder
+                  </div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/amando.jpg"
+                      alt="Amando Boncales"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Amando Boncales</div>
+                  <div className="team-member-position">Althash University, CEO</div>
+                </div>
+              </div>
+              <div role="listitem" className="speaker_item w-dyn-item">
+                <div className="team-card">
+                  <div className="team-member_image-wrapper">
+                    <img
+                      loading="lazy"
+                      src="/imgg/Drew Williams.jpeg"
+                      alt="Drew Williams"
+                      className="team-member-image"
+                    />
+                  </div>
+                  <div className="team-member-name">Drew Williams</div>
+                  <div className="team-member-position">
+                   FANFUNDR, Founder
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+         
         </div>
-        <button type='submit'>Submit</button>
-      </form>
-    </>
+        
+      </div>
+    </div>
+  </div>
+</div>
   )
 }
 
-export default Speaker;
+export default Speaker
