0bada7ca452f3066509b67fba710ac726488fa14
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2024-03-10 00:16:03 +0100

    Update Insights.js

--- src/components/Insights.js
+++ src/components/Insights.js
@@ -30,7 +30,7 @@ function Insights() {
   <div class="p-4" style={{"padding":"1rem"}}>
     <div class="mt-12 w-full max-w-4xl" style={{"marginTop":"3rem","width":"100%","maxWidth":"56rem"}}>
       <h3 class="text-center font-bold my-4 text-white text-3xl lg:text-4xl leading-10 lg:leading-12" style={{"marginTop":"1rem","marginBottom":"1rem","fontSize":"1.875rem","lineHeight":["2.25rem","2.5rem"],"fontWeight":700,"textAlign":"center","color":"#ffffff","@media (min-width: 1024px)":{"fontSize":"2.25rem","lineHeight":"2.5rem"}}}>
-        Highlights from W3LC 2023
+        Highlights from W3LC </br> 2023
       </h3>
       <div class="aspect-ratio-16-9">
         <div class="flex items-center justify-center" style={{"display":"flex","justifyContent":"center","alignItems":"center"}}>
