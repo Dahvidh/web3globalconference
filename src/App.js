16ceb26651cc6c53a08c35e0279d083775fc289e
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2024-05-02 23:53:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import './App.css';
-import Homepage from './components/Homepage';
-import Booking from './components/Booking';
-import Terms from './components/Terms';
-import Sponsor from './components/Sponsor';
-import Privacy from './components/Privacy';
-import Footer from './components/Footer';
-import Payments from './components/Payments';
-import Speaker from './components/Speaker';
-import Header from './components/Header';
-import Speakerlist from './components/Speakerlist';
-import About from './components/About';
-import Insights from './components/Insights';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import { useEffect } from 'react';
+import "./App.css";
+import Homepage from "./components/Homepage";
+import Footer from "./components/Footer";
+import Speaker from "./components/Speaker";
+import Speakerlist from "./components/Speakerlist";
+import About from "./components/About";
+import Reasons from "./components/Reasons";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Homepage/>
-      <About/>
-     <Reasons />
-      <Speaker/>
-      <Speakerlist/>
-      <Footer/>
+      <Homepage />
+      <About />
+      <Reasons />
+      <Speaker />
+      <Speakerlist />
+      <Footer />
     </div>
   );
 }
